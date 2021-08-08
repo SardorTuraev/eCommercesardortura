cd301d8448b6004482d8311cfedc91d0415a65c8
SardorTuraev <sardortura2255@gmail.com>
2021-08-08 22:37:47 +0500

    finished two pages category and index

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-// var elsHeader = document.querySelector('.site-header');
-// var elsHeaderNavBtn = elsHeader.querySelector('.site-header__btn');
+// GRID AND LIST
 
-// if (elsHeaderNavBtn){
-//   elsHeaderNavBtn.addEventListener('click', function() {
-//     elsHeader.classList.toggle('site-header--open');
-//   });
-// }
+var elCatigory = document.querySelector(".catigory__form-dad");
+var elCatigoryBtnGrid = document.querySelector(".catigory__btn-grid");
+var elCatigoryBtnList = document.querySelector(".catigory__btn-list");
+
+if (elCatigoryBtnGrid) {
+  elCatigoryBtnGrid.addEventListener('click', function() {
+    elCatigoryBtnGrid.classList.add('catigory__btn--active');
+    elCatigoryBtnList.classList.remove('catigory__btn--active');
+    elCatigory.classList.add('catigory__product-grid-stile');
+  });
+};
+
+if (elCatigoryBtnList) {
+  elCatigoryBtnList.addEventListener('click', function() {
+    elCatigoryBtnGrid.classList.remove('catigory__btn--active');
+    elCatigoryBtnList.classList.add('catigory__btn--active');
+    elCatigory.classList.remove('catigory__product-grid-stile');
+  });
+};
+
 
 //MODAL SEARCH
 
@@ -28,11 +42,27 @@ if (elHeaderMainForm) {
   });
 }
 
-// window.onclick = function(event) {
-//   if (event.target == elHeaderMainForm) {
-//     elHeaderMainForm.classList.remove('header-main__modal--open');
-//   }
-// }
+// MODAL SHOPPING
+
+var elShopping = document.querySelector('.shopping');
+var elHeaderMainCart = document.querySelector('.header-main__cart');
+var elShoppingHeaderClose = document.querySelector('.shopping__header-close');
+if (elHeaderMainCart) {
+  elHeaderMainCart.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    elShopping.classList.add('shopping--open');
+  });
+}
+
+if (elShoppingHeaderClose) {
+  elShoppingHeaderClose.addEventListener('click', function () {
+    elShopping.classList.remove('shopping--open');
+  });
+}
+
+
+// MENU GAMBURGER
 
 var elHeaderMenuLink = document.querySelector(".header-menu__link");
 var elHeaderMenuBox = document.querySelector(".header-menu__box");
@@ -40,20 +70,47 @@ var elHeaderMenuBox = document.querySelector(".header-menu__box");
 if (elHeaderMenuLink) {
   elHeaderMenuLink.addEventListener('click', function (evt) {
     evt.preventDefault();
-    elHeaderMenuBox.classList.add('header-menu__box--open');
+    elHeaderMenuBox.classList.toggle('header-menu__box--open');
   });
 }
 
-// window.onclick = function(event) {
-//   if (event.target == elHeaderMenuBox) {
-//     elHeaderMenuBox.classList.remove('header-menu__box--open');
-//   }
-// }
+// CLOSE CATIGORY BTN
 
-if (elHeaderMenuBox) {
-  elHeaderMenuBox.addEventListener('click', function (evt) {
-    if (evt.target.matches('.header-menu__box')) {
-      elHeaderMenuBox.classList.remove('header-menu__box--open');
-    }
+var elsCatigoryCheckBtn = document.querySelectorAll(".catigory__check-btn");
+
+elsCatigoryCheckBtn.forEach(function (list) {
+  list.addEventListener('click', function () {
+    list.closest("p").remove();
+  });  
+});
+
+
+var elCatigoryProductMobileBtn = document.querySelector('.catigory__product-mobile-btn');
+var elCatigoryProductsLeft = document.querySelector('.catigory__products-left');
+
+if (elCatigoryProductMobileBtn) {
+  elCatigoryProductMobileBtn.addEventListener('click', function () {
+    elCatigoryProductsLeft.classList.toggle('catigory__settings--open');
   });
 }
+
+
+
+// SHOPPING ACTIVE
+
+var elsShoppingCartWish = document.querySelectorAll('.shopping__cart-left-wish');
+var elsShoppingCartClose = document.querySelectorAll('.shopping__cart-left-remove');
+var elShoppingItem = document.querySelector('.shopping__item');
+
+elsShoppingCartWish.forEach(function (w) {
+  w.addEventListener('click', function () {
+    w.classList.toggle("shopping__cart-wish--active");
+  });
+});
+
+
+elsShoppingCartClose.forEach(function (c) {
+  c.addEventListener('click', function () {
+    c.closest("li").parentElement.parentElement.parentElement.remove();
+  });
+});  
